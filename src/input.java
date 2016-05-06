b8a1196c016613861ab2bc142546a8ad6dc56b68
Nathan Thomsen <nathan.c.thomsen@gmail.com>
2016-05-06 14:43:37 -0700

    Implementing array list of strings.

--- src/input.java
+++ src/input.java
@@ -1,36 +1,8 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class input {
-    private String input;
-
-    public input(String input)
-    {
-    	this.input = input;
-    }
-    
-    /* Use if user asks to encrypt. */
-    public boolean askEncrypt()
-    {
-	if (input.equals("Encrypt") || input.equals("encrypt")
-	    || input.equals("e"))
-	{
-	    return true;
-	}
-	else
-	{
-	    return false;
-	}
-    }
-
-    /* Use if user asks to decrypt. */
-    public boolean askDecrypt()
-    {
-	if (input.equals("Decrypt") || input.equals("decrypt")
-	    || input.equals("d"))
-	{
-	    return true;
-	}
-	else
-	{
-	    return false;
-	}
-    }
+	ArrayList<String> user_inputs = new ArrayList<String>();
+	
+	Scanner scan = new Scanner(System.in);
 }
