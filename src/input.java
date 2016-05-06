bc656a719fc93689ef941a81fdbcc03076242351
NickTheMighty <nicholas.moglia@gmail.com>
2016-05-06 15:01:10 -0700

    input changes

--- src/input.java
+++ src/input.java
@@ -2,7 +2,44 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class input {
-	ArrayList<String> user_inputs = new ArrayList<String>();
+	private ArrayList<String> user_inputs;
 	
-	Scanner scan = new Scanner(System.in);
+	public input(ArrayList<String> theInput)
+	{
+		for(int i = 0; i<theInput.size(); i++)
+		{
+			user_inputs.set(i, theInput.get(i));
+		}
+	}
+	
+	public boolean isEncrypt()
+	{
+		if(user_inputs.get(0).equals("Encrypt")||user_inputs.get(0).equals("encrypt"))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public boolean isDecrypt()
+	{
+		if(user_inputs.get(0).equals("Decrypt")||user_inputs.get(0).equals("decrypt"))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	public String toString()
+	{
+		String output = new String();
+		output += " ";
+		return output;
+	}
+
 }
