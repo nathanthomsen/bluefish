9849b1b6d1250dd8a9b4bb607290aded77e38f6b
Nathan Thomsen <nathan.c.thomsen@gmail.com>
2016-05-06 14:17:27 -0700

    Practice file, will delete at a later time.

---
+++ src/input.java
@@ -0,0 +1,36 @@
+public class input {
+    private String input;
+
+    public Input(String input)
+    {
+	this.input = input;
+    }
+    
+    /* Use if user asks to encrypt. */
+    public boolean askEncrypt
+    {
+	if (input.equals("Encrypt") || input.equals("encrypt")
+	    || input.equals("e"))
+	{
+	    return true;
+	}
+	else
+	{
+	    return false;
+	}
+    }
+
+    /* Use if user asks to decrypt. */
+    public boolean askDecrypt()
+    {
+	if (input.equals("Decrypt") || input.equals("decrypt")
+	    || input.equals("d"))
+	{
+	    return true;
+	}
+	else
+	{
+	    return false;
+	}
+    }
+}
