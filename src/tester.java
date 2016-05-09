c00618891b00736ca54c13ce81c0475624e5a88f
Nathan Thomsen <nathan.c.thomsen@gmail.com>
2016-05-09 13:46:08 -0700

    Cleaned up code.

--- src/tester.java
+++ src/tester.java
@@ -1,23 +1,23 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class tester {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		String scanned = scan.next();
+		
 		ArrayList<String> user_inputs = new ArrayList<String>();
 		user_inputs.add(scanned);
+		
 		input theInput = new input(user_inputs);
-		if(theInput.isEncrypt())
+		
+		if(theInput.askEncrypt())
 		{
-			//Call Encrypt method
+			/* TODO Call Encrypt method */
 		}
-		else if(theInput.isDecrypt())
+		else if(theInput.askDecrypt())
 		{
-			//Call Decrypt method
+			/* TODO Decrypt method */
 		}
 	}
 
